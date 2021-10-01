375beffe2febb0833747d8965db9ef8700bc6c11
rjhee <qkdrkwogml@gmail.com>
2021-10-01 15:21:27 +0900

    font size change, images add

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(function () {
-  // pc버전 html background-img 높이
-  let allHeight = $('html').outerHeight();
-  $('.background-img-pc').css('height', `${allHeight}px`);
-
   // pc버전 매거진 리스트에 마우스 오버시 이미지 보이기
   for (let order = 0; order < 6; order++) {
     $('.magazine-list')
@@ -64,21 +60,31 @@ $(function () {
     return false;
   });
 
-  // 모바일버전 하단 background-image 높이
-  let collectionH = $('.collection').outerHeight();
-  let magazineH = $('.magazine').outerHeight();
-  let footerH = $('footer').outerHeight();
+  // pc버전 html background-img 높이
+  let allHeight = $('html').outerHeight();
+  $('.background-img-pc').css('height', `${allHeight}px`);
 
-  $('.background-img-tablet').css(
-    'height',
-    `${collectionH + magazineH + footerH + 300}px`
-  );
+  $(window).resize(function () {
+    // pc버전 html background-img 높이
+    let allHeight = $('html').outerHeight();
+    $('.background-img-pc').css('height', `${allHeight}px`);
+
+    // 모바일버전 하단 background-image 높이
+    let collectionH = $('.collection').outerHeight();
+    let magazineH = $('.magazine').outerHeight();
+    let footerH = $('footer').outerHeight();
+
+    $('.background-img-tablet').css(
+      'height',
+      `${collectionH + magazineH + footerH + 300}px`
+    );
+  });
 
   // recycle 스크롤top 자석처럼 붙이기
   $(window).scroll(function () {
     let recycleTop = $('.main-recycle').offset().top;
     let userScrollTop = $(this).scrollTop();
-    console.log(recycleTop);
+
     $('.main-checkers').on('mousewheel', function (e) {
       var wheel = e.originalEvent.wheelDelta;
 
@@ -101,6 +107,7 @@ $(function () {
     });
   });
 
+  // 모바일 버전 삼단바메뉴 클릭시 메뉴 보이기
   $('.hamburger-btn').click(function () {
     let nav = $('.nav-cover').css('display');
 
@@ -112,12 +119,27 @@ $(function () {
   $('.close-btn').click(function () {
     $('.nav-cover').css('display', 'none');
   });
-});
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 660) {
-    $('.nav-cover').css('display', 'flex');
-  } else {
+  // search 버튼 클릭시 검색창 보이기
+
+  $('.search-menu-btn').click(function () {
+    $('.search').css('display', 'flex');
     $('.nav-cover').css('display', 'none');
-  }
+  });
+
+  $('.search-close-btn').click(function () {
+    $('.search').css('display', 'none');
+    if (window.innerWidth > 660) {
+      $('.nav-cover').css('display', 'flex');
+    }
+  });
+
+  // 윈도우 사이즈가 변경될때 네비게이션 메뉴 없애고 보이기
+  $(window).resize(function () {
+    if (window.innerWidth > 660) {
+      $('.nav-cover').css('display', 'flex');
+    } else {
+      $('.nav-cover').css('display', 'none');
+    }
+  });
 });
